194e6749669478b23196872f17198a3e67d67c54
Maulidin Zakaria <maulidinzakaria123@gmail.com>
2024-10-22 21:32:41 +0700

    add draggable widget to show result of check (camera)

--- lib/pages/check/check_page.dart
+++ lib/pages/check/check_page.dart
@@ -1,5 +1,3 @@
-import 'dart:io';
-
 import 'package:camera/camera.dart';
 import 'package:flutter/material.dart';
 import 'package:frontend_mobile_tugasbesar/pages/check/display_picture_screen.dart';
