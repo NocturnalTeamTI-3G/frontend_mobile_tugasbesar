980d1a53521bb15490eba013aaaece0593c78887
abidgymnastiar <abidgymnastiar@gmail.com>
2024-11-07 05:22:38 +0700

    home

--- lib/app/modules/home/home_page.dart
+++ lib/app/modules/home/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
+import 'package:frontend_mobile_tugasbesar/app/widgets/custom_appbar.dart';
 import 'package:google_fonts/google_fonts.dart';
 
 class MainHomePage extends StatefulWidget {
@@ -15,67 +16,52 @@ class _mainHomePageState extends State<MainHomePage> {
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
-      body: Column(
-        children: [
-          Container(
-              padding: EdgeInsets.symmetric(horizontal: 24),
+      appBar: const PreferredSize(
+        preferredSize: Size.fromHeight(100),
+        child: CustomAppbar(
+          title: 'Home',
+        ),
+      ),
+      body: SafeArea(
+        child: Column(
+          children: [
+            const SizedBox(height: 25),
+            Container(
+              padding: const EdgeInsets.only(left: 20),
+              decoration: BoxDecoration(
+                color: Colors.pink[100],
+              ),
               child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
                 children: [
-                  Row(
+                  // animation of cute pictures
+                  Container(
+                    child: Container(
+                      height: 100,
+                      width: 100,
+                      color: Colors.deepPurple,
+                    ),
+                  ),
+                  // how do you feel today?
+                  Column(
                     children: [
+                      Text(
+                        "How do you feel?",
+                      ),
+                      Text(
+                        "Fill out your medical card right now!",
+                      ),
                       Container(
-                        width: 60,
-                        height: 60,
-                        padding: const EdgeInsets.all(4),
-                        decoration: BoxDecoration(
-                          color: Colors.white,
-                          border: Border.all(
-                            color: const Color.fromARGB(255, 53, 40, 40),
-                          ),
-                          borderRadius: BorderRadius.circular(60 / 2),
+                        child: Center(
+                          child: Text("Get started"),
                         ),
-                        child: ClipOval(
-                          child: Image.asset(
-                            'assets/images/profile.jpg',
-                            fit: BoxFit.cover,
-                          ),
-                        ),
-                      ),
-                      SizedBox(
-                        width: 12,
-                      ),
-                      Column(
-                        crossAxisAlignment: CrossAxisAlignment.start,
-                        children: [
-                          Text(
-                            'Abid Gymnasium',
-                            style: GoogleFonts.roboto(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 18,
-                                color: Colors.black),
-                          ),
-                          Text(
-                            'TI-3G',
-                            style: GoogleFonts.roboto(
-                                fontWeight: FontWeight.w500,
-                                fontSize: 14,
-                                color: const Color.fromARGB(255, 69, 68, 68)),
-                          ),
-                        ],
                       )
                     ],
                   ),
-                  CircleAvatar(
-                    backgroundColor: Colors.white,
-                    child: IconButton(
-                      onPressed: () {},
-                      icon: SvgPicture.asset('assets/svgs/notification.svg'),
-                    ),
-                  )
                 ],
-              ))
-        ],
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
